31b93ead35ec397f3fda6a73a1033809f7745b41
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2021-06-28 13:19:10 -0300

    initial commit

---
+++ ByteBankWPF/ByteBank.Agencias/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using ByteBank.Agencias.DAL;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ByteBank.Agencias
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ByteBankEntities _contextoBancoDeDados = new ByteBankEntities();
+        private readonly ListBox lstAgencias;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lstAgencias = new ListBox();
+            AtualizarControles();
+        }
+
+        private void AtualizarControles()
+        {
+            lstAgencias.Width = 270;
+            lstAgencias.Height = 290;
+
+            Canvas.SetTop(lstAgencias, 15);
+            Canvas.SetLeft(lstAgencias, 15);
+
+            lstAgencias.SelectionChanged += new SelectionChangedEventHandler(lstAgencias_SelectionChanged);
+
+            container.Children.Add(lstAgencias);
+
+            lstAgencias.Items.Clear();
+            var agencias = _contextoBancoDeDados.Agencias.ToList();
+            foreach (var agencia in agencias)
+                lstAgencias.Items.Add(agencia);
+        }
+        private void lstAgencias_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var confirmacao = 
+                MessageBox.Show(
+                    "Você deseja realmente excluir este item?",
+                    "Confirmação",
+                    MessageBoxButton.YesNo);
+            if (confirmacao == MessageBoxResult.Yes)
+            {
+                //Excluir
+            }
+            else
+            {
+                //Não faz nada
+            }
+        }
+    }
+}
